9acd593503ebb44d1a7c0e17262abcc2f7965406
23-Nikita <mittalnikita.23@gmail.com>
2026-03-12 00:59:22 +0530

    Model: Update listing schema to include category and geometry fields

--- models/listing.js
+++ models/listing.js
@@ -28,6 +28,10 @@ const listingSchema = new Schema({
     price: Number,
     location: String,
     country: String,
+    category: {
+      type: String,
+      enum: ["Trending", "Rooms", "Iconic Cities", "Mountains", "Castles", "Amazing Pools", "Camping", "Farms", "Arctic", "Domes", "Beach"]
+    },
     reviews: [
   {
     type:Schema.Types.ObjectId,
